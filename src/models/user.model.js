03db7910976a7703931a93c867b0719fa1944ff9
Karoleen Amin <karolamin47@gmail.com>
2026-03-26 11:18:14 +0200

    add user Model

---
+++ src/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;

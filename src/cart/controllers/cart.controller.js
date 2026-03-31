e3a2077ff2d1fa7d67d2fbfca1e4d9ef85902f08
Eslam Mohamed <eslama202002@gmail.com>
2026-03-31 09:44:02 +0200

    fix gitignore

--- src/cart/controllers/cart.controller.js
+++ src/cart/controllers/cart.controller.js
@@ -1,19 +1,50 @@
 import { successResponse } from "../../utils/apiResponse.js";
 import { AppError, catchAsync } from "../../utils/errorHandler.js";
 import cartModel from "../models/cart.model.js";
-export const getCart = catchAsync(async (req, res,next) => {
+
+
+export const getCart = catchAsync(async (req, res, next) => {
   let cart = await cartModel
     .findOne({ user: req.user.id })
     .populate("items.product");
-    if (!cart) {
-  return successResponse(res, 200, "Cart is empty", { items: [] });
-}
-    return successResponse(res, 200, "Cart Products fetched successfully", cart)
-})
+
+  if (!cart) {
+    return successResponse(res, 200, "Cart is empty", {
+      items: [],
+      totalPrice: 0,
+    });
+  }
+
+  const totalPrice = cart.items.reduce((acc, item) => {
+    return acc + item.quantity * item.product.price;
+  }, 0);
+
+  return successResponse(
+    res,
+    200,
+    "Cart Products fetched successfully",
+    {
+      cart,
+      totalPrice,
+    }
+  );
+});
+
+// export const getCart = catchAsync(async (req, res,next) => {
+//   let cart = await cartModel
+//     .findOne({ user: req.user.id })
+//     .populate("items.product");
+//     if (!cart) {
+//   return successResponse(res, 200, "Cart is empty", { items: [] });
+// }
+//     return successResponse(res, 200, "Cart Products fetched successfully", cart)
+// })
+
+
 export const addToCart = catchAsync(async (req, res,next) => {
   let { productId, quantity } = req.body;
   quantity = quantity || 1;
-  let cart = await cartModel.findOne({ user: req.user.id });
+  let cart = await cartModel.findOne({ user: req.user.id }).populate("items.product");
   if (!cart) {
     cart = await cartModel.create({
       user: req.user.id,
